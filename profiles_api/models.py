15ddb6d4d4747ffb4fdb8c8058627f0a35379c45
vinish <vinishbhan11@gmail.com>
2021-07-30 18:26:29 +0530

    Wnable Django Admin for the user Profile

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -9,17 +9,17 @@ class UserProfileManager(BaseUserManager):
     def create_user(self,email,name,password=None):
         """Create a new user profile"""
         if not email:
-            raise ValueError("User must have an email address")
+            raise ValueError('User must have an email address')
 
-        email.self.normalize_email(email)
-        user = self.model(email=email,name=name)
+        email  = self.normalize_email(email)
+        user = self.model(email=email, name=name)
 
         user.set_password(password)
         user.save(using=self._db)
         return user
 
 
-    def create_super_user(self,email,name,password):
+    def create_superuser(self,email,name,password):
         """Create and save a new superuser with given details"""
 
         user = self.create_user(email,name,password)
